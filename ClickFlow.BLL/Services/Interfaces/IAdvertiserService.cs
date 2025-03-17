eabe3a5fbbbd84f4a734807c8039229caff4a799
khaiqui <nguyenkhaiqui2003@gmail.com>
2025-03-17 12:42:33 +0700

    Fix create campaign

--- ClickFlow.BLL/Services/Interfaces/IAdvertiserService.cs
+++ ClickFlow.BLL/Services/Interfaces/IAdvertiserService.cs
@@ -1,10 +1,11 @@
 ﻿using ClickFlow.BLL.DTOs.AdvertiserDTOs;
+using ClickFlow.DAL.Entities;
 using ClickFlow.DAL.Paging;
 
 namespace ClickFlow.BLL.Services.Interfaces
 {
 	public interface IAdvertiserService
 	{
-
+        Task<Advertiser> GetAdvertiserByUserIdAsync(int userId);
     }
 }
