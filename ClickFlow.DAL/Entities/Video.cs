3d959c6f269772a4cc1f72bc5da67c6316d64ed1
N-I-V-Z <vothanhnhan106@gmail.com>
2025-05-10 20:09:21 +0700

    remove Q&A table

--- ClickFlow.DAL/Entities/Video.cs
+++ ClickFlow.DAL/Entities/Video.cs
@@ -5,9 +5,7 @@
 		public int Id { get; set; }
 		public string Link { get; set; }
 		public string Title { get; set; }
-		public string Duration { get; set; }
 		public int CourseId { get; set; }
 		public Course Course { get; set; }
-		public ICollection<QuestionAndAnswer> QuestionAndAnswers { get; set; }
 	}
 }
