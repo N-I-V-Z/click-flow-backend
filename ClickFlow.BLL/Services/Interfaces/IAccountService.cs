7bf2679d7a8fd39d0e742896ebbd4f270403f135
khaiqui <nguyenkhaiqui2003@gmail.com>
2025-06-11 00:38:45 +0700

    fix renew token

--- ClickFlow.BLL/Services/Interfaces/IAccountService.cs
+++ ClickFlow.BLL/Services/Interfaces/IAccountService.cs
@@ -13,6 +13,7 @@ namespace ClickFlow.BLL.Services.Interfaces
         Task<BaseResponse> SendOTP2FA(ApplicationUser user, string password);
         Task<BaseResponse> SignOutAsync(SignOutDTO signOutDTO);
         Task<BaseResponse> CheckToRenewTokenAsync(AuthenResultDTO authenResult);
+        Task<AuthenResultDTO> GenerateTokenFromRefreshTokenAsync(AuthenResultDTO authenResult);
         Task<BaseResponse> ForgotPasswordAsync(AccountForgotPasswordDTO dto);
         Task<BaseResponse> ResetPasswordAsync(AccountResetpassDTO dto);
         Task<BaseResponse> SendOTPByEmailAsync(string email);
