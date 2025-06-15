4017956ec08b8d7b8bfbf92d62367302eca743f0
khaiqui <nguyenkhaiqui2003@gmail.com>
2025-06-15 23:28:12 +0700

    add id to response user

--- ClickFlow.BLL/DTOs/ApplicationUserDTOs/ApplicationUserResponseDTO.cs
+++ ClickFlow.BLL/DTOs/ApplicationUserDTOs/ApplicationUserResponseDTO.cs
@@ -6,6 +6,7 @@ namespace ClickFlow.BLL.DTOs.ApplicationUserDTOs
 {
 	public class ApplicationUserResponseDTO
 	{
+		public int Id { get; set; }
 		public string FullName { get; set; }
 		public Role Role { get; set; }
 		public bool IsDeleted { get; set; }
